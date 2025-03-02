fc4d0f60229b81afe05fdb51229e609b3ba76098
Zarubin Alexander <zarubin_14@list.ru>
2025-03-02 15:22:30 +0300

    chore(wether panel): init component

---
+++ src/components/WeatherPanel.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-container>
+        <v-card>
+            <v-card-title>Погода</v-card-title>
+            <v-card-text>
+                <v-text-field label="Введите город"></v-text-field>
+            </v-card-text>
+        </v-card>
+    </v-container>
+</template>
+
+<script></script>
